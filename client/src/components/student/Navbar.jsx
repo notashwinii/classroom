2806c022339e86ef3f0e06085b62f973d899b25a
notashwinii <ashwinii.subedi@gmail.com>
2025-01-22 18:46:36 +0545

    update

--- client/src/components/student/Navbar.jsx
+++ client/src/components/student/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import {Link} from "react-router-dom";
 import Logo from "/KU-CMS.webp";
 
 const Navbar = ({ isOpen, setIsOpen }) => {
@@ -8,10 +8,15 @@ const Navbar = ({ isOpen, setIsOpen }) => {
     <div className="fixed top-0 left-0 w-full bg-white shadow-md z-50 px-6 py-4 flex justify-between items-center">
       
     {/* Left Side: Logo and Name */}
+    
+    <Link to="/student/dashboard">
     <div className="flex items-center space-x-4">
       <img src={Logo} alt="KU-CMS Logo" className="h-10 w-auto" />
       <h1 className="ml-3 text-xl font-semibold text-gray-800">KU-CMS</h1>
-    </div>
+    
+      </div>
+      </Link>
+    
 
     {/* Right Side: "Hello, Student" */}
     <div className="flex items-center text-gray-800">
--- client/src/components/student/sidebar.jsx
+++ client/src/components/student/sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Book, FileText, ChevronRight, Bell } from "lucide-react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { Link } from "react-router-dom"; 
 
 const Sidebar = ({ sidebarOpen }) => {
   return (
